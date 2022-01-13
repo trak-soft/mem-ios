f26728805321adc3cc491555732a71c9761c0d39
Emmanuel Kareem <ekareem@finishline.com>
2022-01-13 08:06:42 -0500

    create empty projcet with Unit test

---
+++ mem-ios/mem_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mem_iosApp.swift
+//  mem-ios
+//
+//  Created by Emmanuel Kareem on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct mem_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
