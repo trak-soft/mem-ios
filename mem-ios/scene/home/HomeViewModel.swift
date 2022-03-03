233c21abd49038d9f92c218ff7dc75e52411dd1d
Emmanuel Kareem <ekareem@finishline.com>
2022-03-02 23:35:18 -0500

    MEM-49 add colors to card

--- mem-ios/scene/home/HomeViewModel.swift
+++ mem-ios/scene/home/HomeViewModel.swift
@@ -30,7 +30,7 @@ class HomeViewModel: ObservableObject {
         OptionType.MODE(groupLength: 2,  preview: false, numOfGroup: 15, timeLimit: 2, clickLimit: 2),
         OptionType.MODE(groupLength: 4,  preview: false, numOfGroup: 8, timeLimit: 2, clickLimit: 2),
         OptionType.MODE(groupLength: 3,  preview: false, numOfGroup: 7, timeLimit: 2, clickLimit: 2),
-        OptionType.MODE(groupLength: 2,  preview: false, numOfGroup: 7, timeLimit: 2, clickLimit: 2),
+        OptionType.MODE(groupLength: 2,  preview: false, numOfGroup: 7, timeLimit: nil, clickLimit: nil),
         OptionType.MODE(groupLength: 2,  preview: true, numOfGroup: 3, timeLimit: 20, clickLimit: 50),
     ]
 
